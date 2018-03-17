c7137aa4f6f559e64dc4936536d1bd1d76b80ba3
zlice <zlice555@gmail.com>
2018-03-17 11:03:23 -0700

    revamp, clear/clean up

--- 4ROLL.c
+++ 4ROLL.c
@@ -1,47 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 /*
-  table 'key' has 4 bases - 6 perms = 24 total size
+  table 'key' has 4 bases and 24 total size with permutations
   4 bases = 1byte (8bits)
-  encode base and 'roll'
+  encode base and 'roll' table
   roll = 0-23 table (24 values)
-       each roll adds 6 (changes 'low' encoding bit)
-       add 1-4 depending on last base encoded
-       add determined from 'key table' where the +6 landed
+       each roll adds 6 (changes encoding)
+       roll depending on last base encoded
+       (^NOTE: may want to change to non-data related roll to avoid massive errors when decoding)
 
- base4 (data to encode is 43dec (0x2B) )
- table 'key'
-     : ACGT - A=0, C=1, G=2, T=3
- encoded A for high end of data (0 decimal. 3000base4 is 192base10)
- table jump 6 ahead (lands between 0-23)
- tmp table says
-     : C=0, A=1, T=2, G=3
- last data encoded was A, so add 1 more
- table 'key'
-     : C=0, G=1, T=2, A=3
- data : AT__ (02__)
- table jump 6 ahead
- tmp table says
-     : G=0, A=1, T=2, C=3
- last data encoded was T, so add 2 more
- next encode will be the format of
- table 'key'
-     : G=0, C=1, T=2, A=3
- data : ATT_ (022_)
- table jump 6 ahead (back to [0])
- tmp table says
-     : T=0, C=1, A=2, G=3
- last data encoded was T, so add 1 more
- next encode will be the format of
- table 'key'
-     : T=0, G=1, C=2, A=3
- data : ATTA (0223)
- final data: ATTA (0223) or 43dec or '+' or 0x2B :)
+encode method:
+encoded byte 00101111 (0x2F in hex, or '/' in ascii)
+break binary into sections 00-10-11-11
+use table to encode
+ACGT
+A = 00
+C = 01
+G = 10
+T = 11
+
+00-10-11-11
+^start here
+
+A = 00
+add 'A' to output
+'roll' through table
+
+00-10-11-11
+   ^now here
+
+repeat
+
+exceptions: homopolymer nucleotide repeats
+when HOMO_REPEATS is hit, add bs (bit shift, bullshit, whichever) to buffer
+
 */
+
 //32bit x 24 array
-const u_int g4ROLL [24] = {
+const u_int G4ROLL [24] = {
 (u_int)( (u_char)'A'<<24 | (u_char)'C'<<16 | (u_char)'G'<<8 | (u_char)'T'),
 (u_int)( (u_char)'A'<<24 | (u_char)'C'<<16 | (u_char)'T'<<8 | (u_char)'G'),
 (u_int)( (u_char)'A'<<24 | (u_char)'G'<<16 | (u_char)'C'<<8 | (u_char)'T'),
@@ -67,166 +66,131 @@ const u_int g4ROLL [24] = {
 (u_int)( (u_char)'T'<<24 | (u_char)'G'<<16 | (u_char)'C'<<8 | (u_char)'A'),
 (u_int)( (u_char)'T'<<24 | (u_char)'G'<<16 | (u_char)'A'<<8 | (u_char)'C')
 };
+const u_char HOMO_REPEAT = 3;
 
-int gTblPos=0, gTblAdd = 0;//gTblPos should be 0-23, gTblAdd should be 1-4
+int g_tbl_pos=0, g_tbl_add = 0;//gTblPos should be 0-23
 
-void tblSet(u_char pLast) { //time for dinner!
-     gTblPos += 6;
-     if(gTblPos > 23)
-       gTblPos -= 24;
+void tblSet(u_char p_last) { //time for dinner!
+     g_tbl_pos += 6;
+     if(g_tbl_pos > 23)
+       g_tbl_pos -= 24;
 
-     if      (pLast == (u_char) (g4ROLL[gTblPos]) )
-       gTblAdd=1;
-     else if(pLast == (u_char) (g4ROLL[gTblPos])>>8)
-       gTblAdd=2;
-     else if(pLast == (u_char) (g4ROLL[gTblPos])>>16)
-       gTblAdd=3;
-     else if(pLast == (u_char) (g4ROLL[gTblPos])>>24)
-       gTblAdd=4;
+     if      (p_last == (u_char) (G4ROLL[g_tbl_pos]) )
+       g_tbl_add=3;
+     else if(p_last == (u_char) (G4ROLL[g_tbl_pos])>>8)
+       g_tbl_add=1;
+     else if(p_last == (u_char) (G4ROLL[g_tbl_pos])>>16)
+       g_tbl_add=2;
+     else if(p_last == (u_char) (G4ROLL[g_tbl_pos])>>24)
+       g_tbl_add=6;
 
-     gTblPos += gTblAdd;
-     if(gTblPos > 23)
-       gTblPos -= 24;
-     if(gTblPos > 23) {
+     g_tbl_pos += g_tbl_add;
+     if(g_tbl_pos > 23)
+       g_tbl_pos -= 24;
+     if(g_tbl_pos > 23) {
        printf("ERROR! table pos outside bounds!\nEXITING!\n");
-       return 0;
+       return;
      }//if
 
-     //printf("new gTblPos is %d - output was %c\n", gTblPos, pLast);
+     //printf("new g_tbl_pos is %d - output was %c\n", gTblPos, p_last);
 }//tblSet
 
 
 int main() {
 
-  char * fileBuf;
-  u_char * outBuf;
-  int fileSz=0, outCnt=0, i=0;
-  u_char dtEnc = 0;//read byte by byte
-  FILE *readFile;
+  char * file_buf;
+  u_char * out_buf, lst_nuc=0, b1=0, b2=0, b3=0;//b4=0;
+  long long file_sz=0, i=0, k=0, n_cnt=1, bs_offset=0, shift=0;
+  u_char dt_enc = 0;//read byte by byte
+  FILE *read_file;
 
 /*
   printf("using table\n");
   for(i=0;i<24;i++) {
-   printf("%d = %c%c%c%c\n", i, g4ROLL[i]>>24, g4ROLL[i]>>16, g4ROLL[i]>>8, g4ROLL[i]);
+   printf("%d = %c%c%c%c\n", i, G4ROLL[i]>>24, G4ROLL[i]>>16, G4ROLL[i]>>8, G4ROLL[i]);
   }//for 24 (i)
 */
 
-  readFile = fopen("/tmp/test", "r");
-  fseek(readFile, 0L, SEEK_END);
-  fileSz = ftell(readFile);
-  rewind(readFile);
+  read_file = fopen("/tmp/test", "r");
+  fseek(read_file, 0L, SEEK_END);
+  file_sz = ftell(read_file);
+  rewind(read_file);
 
-  fileBuf = malloc(fileSz+1);
-  outBuf  = malloc(fileSz*4+1);
-  memset(fileBuf, 0x0, fileSz+1);
-  memset(outBuf, 0x0, fileSz*4+1);
-  fread(fileBuf, fileSz, 1, readFile);
+  file_buf = malloc(file_sz+1);
+  out_buf  = malloc(file_sz*5+1);
+  //*5 for bs_offset safety
+  //unlikely to be full of XXX repeats
+  //should only need 25% increase in size
+  //max 30%(?) XXXYYYZZZ
+  //           rrrGrrGrr (G=garbage,r=real)
+  memset(file_buf, 0x0, file_sz+1);
+  memset(out_buf, 0x0, file_sz*5+1);
+  fread(file_buf, file_sz, 1, read_file);
 
-  //printf("%s\n---EOF---\n", fileBuf);
-
-  for(i=0; i<fileSz; i++) {
+  for(i=0; i<file_sz; i++) {
      //printf("filePos is %d\n", i);
-     dtEnc = fileBuf[i];
-     //block 1 (high) (0xFF, 0x00, 0x00, 0x00)
-     if(dtEnc > 191) {
-       outBuf[outCnt*4] = (u_char)((g4ROLL[gTblPos]));
-       dtEnc -= 192;
-     }
-     else if(dtEnc > 127) {
-       outBuf[outCnt*4] = (u_char)((g4ROLL[gTblPos])>>8);
-       dtEnc -= 128;
-     }
-     else if(dtEnc > 63) {
-       outBuf[outCnt*4] = (u_char)((g4ROLL[gTblPos])>>16);
-       dtEnc -= 64;
-     }
-     else {
-       outBuf[outCnt*4] = (u_char)((g4ROLL[gTblPos])>>24);
-     }//else
-     tblSet(outBuf[outCnt*4]);
+     dt_enc = file_buf[i];
 
+     for(k=0; k<4; k++) {
+          //00-00-00-00 byte encoded
+          b1 = (3<<(2*(3-k)));// 11
+          b2 = (2<<(2*(3-k)));// 10
+          b3 = (1<<(2*(3-k)));// 01
+          //b4 = 0;              00
+          //  k == 0 -  1 -  2 -  3
+          //take  00 - 10 - 11 - 11
+          //check 00, add nucleotide
+          //check 10, add nuc...
+          if(dt_enc >= b1 ) {      //>= 192,48,12,3
+            dt_enc -= b1;
+            //shift = 0;
+          }
+          else if(dt_enc >= b2 ) { //>=  128,32,8,2
+            dt_enc -= b2;
+            shift = 8;
+          }
+          else if(dt_enc >= b3 ) { //>=  64,16,4,1
+            dt_enc -= b3;
+            shift = 16;
+          }
+          else { //00b
+            shift = 24;
+          }//else
+          out_buf[i*4+k+bs_offset] = (u_char)((G4ROLL[g_tbl_pos])>>shift);
+          //printf("coded %c from TBLSET = %d\n", out_buf[i*4+k+bs_offset], g_tbl_pos);
+          if(lst_nuc == out_buf[i*4+k+bs_offset])
+            n_cnt++;
+          else
+            n_cnt = 1;
+          if(n_cnt >= HOMO_REPEAT) {
+            if(shift>7) shift -= 8;
+            else        shift += 8;
+            bs_offset++;
+            out_buf[i*4+k+bs_offset] = (u_char)((G4ROLL[g_tbl_pos])>>shift);
+            //^add bs value, problem is this rolls the table next
+            //could lead to huge errors from corrupt input in decode
+            //ALSO: using non-data value for 'roll' could allow for multi-threading
+            n_cnt = 1;
+          }
+          lst_nuc = out_buf[i*4+k+bs_offset];
+          tblSet(lst_nuc);
+          shift = 0;
+     }//k
 
-     //block 2 (high) (0x00, 0xFF, 0x00, 0x00)
-     if(dtEnc > 47) {
-       outBuf[outCnt*4+1] = (u_char)((g4ROLL[gTblPos]));
-       dtEnc -= 48;
-     }
-     else if(dtEnc > 31) {
-       outBuf[outCnt*4+1] = (u_char)((g4ROLL[gTblPos])>>8);
-       dtEnc -= 32;
-     }
-     else if(dtEnc > 15) {
-       outBuf[outCnt*4+1] = (u_char)((g4ROLL[gTblPos])>>16);
-       dtEnc -= 16;
-     }
-     else {
-       outBuf[outCnt*4+1] = (u_char)((g4ROLL[gTblPos])>>24);
-     }//else
-     tblSet(outBuf[outCnt*4+1]);
-
-
-     //block 3 (low) (0x00, 0x00, 0xFF, 0x00)
-     if(dtEnc > 11) {
-       outBuf[outCnt*4+2] = (u_char)((g4ROLL[gTblPos]));
-       dtEnc -= 12;
-     }
-     else if(dtEnc > 7) {
-       outBuf[outCnt*4+2] = (u_char)((g4ROLL[gTblPos])>>8);
-       dtEnc -= 8;
-     }
-     else if(dtEnc > 3) {
-       outBuf[outCnt*4+2] = (u_char)((g4ROLL[gTblPos])>>16);
-       dtEnc -= 4;
-     }
-     else {
-       outBuf[outCnt*4+2] = (u_char)((g4ROLL[gTblPos])>>24);
-     }//else
-     tblSet(outBuf[outCnt*4+2]);
-
-
-     //block 4 (low) (0x00, 0x00, 0x00, 0xFF)
-     if(dtEnc > 2) {
-       outBuf[outCnt*4+3] = (u_char)((g4ROLL[gTblPos]));
-       //dtEnc -= 3;
-     }
-     else if(dtEnc > 1) {
-       outBuf[outCnt*4+3] = (u_char)((g4ROLL[gTblPos])>>8);
-       //dtEnc -= 2;
-     }
-     else if(dtEnc > 0) {
-       outBuf[outCnt*4+3] = (u_char)((g4ROLL[gTblPos])>>16);
-       //dtEnc -= 1;
-     }
-     else {
-       outBuf[outCnt*4+3] = (u_char)((g4ROLL[gTblPos])>>24);
-     }//else
-
-     /*
-      this should make data repeats longer for DNA structure.
-      if needed, you could shuffle the 'key' table for even
-      more repeat avoidance, while being able to encode/decode.
-
-      i looked at another 'proven' method and there are repeats
-      like this produces in the same encoded data.
-      shuffle is likely a waste
-
-      mathmatical probablity could produce repeats
-     */
-     tblSet(outBuf[outCnt*4+3]);
-
-     outCnt++;
-     if(outCnt % 24)
-       gTblPos += 7;
-     if(gTblPos>23)
-       gTblPos -= 24;
+     //just some more rolling 'randomness'
+     if(i % 24)
+       g_tbl_pos += 7;
+     if(g_tbl_pos>23)
+       g_tbl_pos -= 24;
 
   };//i, file
 
-//printf("DNA ENCODED DATA:\n%s\n---EOF---\n", outBuf);
-  printf("%s",outBuf);
-  free(fileBuf);
-  free(outBuf);
-  fclose(readFile);
+//printf("DNA ENCODED DATA:\n%s\n---EOF---\n", out_buf);
+  printf("%s",out_buf);
+//printf("\nbs_offset = %d\n", bs_offset);
+  free(file_buf);
+  free(out_buf);
+  fclose(read_file);
   return 0;
 
 }//main
--- 4ROLLdec.c
+++ 4ROLLdec.c
@@ -1,46 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
-  table 'key' has 4 bases - 6 perms = 24 total size
+  table 'key' has 4 bases and 24 total size with permutations
   4 bases = 1byte (8bits)
-  encode base and 'roll'
+  dencode base and 'roll' table
   roll = 0-23 table (24 values)
-       each roll adds 6 (changes 'low' encoding bit)
-       add 1-4 depending on last base encoded
-       add determined from 'key table' where the +6 landed
+       each roll adds 6 (changes dencoding)
+       roll depending on last base dencoded
+       (^NOTE: may want to change to non-data related roll to avoid massive errors when decoding)
 
- base4 (data to encode is 43dec (0x2B) )
- table 'key'
-     : ACGT - A=0, C=1, G=2, T=3
- encoded A for high end of data (0 decimal. 3000base4 is 192base10)
- table jump 6 ahead (lands between 0-23)
- tmp table says
-     : C=0, A=1, T=2, G=3
- last data encoded was A, so add 1 more
- table 'key'
-     : C=0, G=1, T=2, A=3
- data : AT__ (02__)
- table jump 6 ahead
- tmp table says
-     : G=0, A=1, T=2, C=3
- last data encoded was T, so add 2 more
- next encode will be the format of
- table 'key'
-     : G=0, C=1, T=2, A=3
- data : ATT_ (022_)
- table jump 6 ahead (back to [0])
- tmp table says
-     : T=0, C=1, A=2, G=3
- last data encoded was T, so add 1 more
- next encode will be the format of
- table 'key'
-     : T=0, G=1, C=2, A=3
- data : ATTA (0223)
- final data: ATTA (0223) or 43dec or '+' or 0x2B :)
+dencode method:
+dencoded byte 00101111 (0x2F in hex, or '/' in ascii)
+break binary into sections 00-10-11-11
+use table to dencode
+ACGT
+A = 00
+C = 01
+G = 10
+T = 11
+
+00-10-11-11
+^start here
+
+A = 00
+add '0' to output (no nothing)
+'roll' through table
+
+00-10-11-11
+   ^now here
+
+repeat
+
+exceptions: homopolymer nucleotide repeats
+when HOMO_REPEATS is hit, ignore bs (bit shift, bullshit, whichever) in buffer
+
 */
 //32bit x 24 array
-const u_int g4ROLL [24] = {
+const u_int G4ROLL [24] = {
 (u_int)( (u_char)'A'<<24 | (u_char)'C'<<16 | (u_char)'G'<<8 | (u_char)'T'),
 (u_int)( (u_char)'A'<<24 | (u_char)'C'<<16 | (u_char)'T'<<8 | (u_char)'G'),
 (u_int)( (u_char)'A'<<24 | (u_char)'G'<<16 | (u_char)'C'<<8 | (u_char)'T'),
@@ -66,140 +64,112 @@ const u_int g4ROLL [24] = {
 (u_int)( (u_char)'T'<<24 | (u_char)'G'<<16 | (u_char)'C'<<8 | (u_char)'A'),
 (u_int)( (u_char)'T'<<24 | (u_char)'G'<<16 | (u_char)'A'<<8 | (u_char)'C')
 };
+const u_char HOMO_REPEAT = 3;
 
-int gTblPos=0, gTblAdd = 0;//gTblPos should be 0-23, gTblAdd should be 1-4
+int g_tbl_pos=0, g_tbl_add = 0;//g_tbl_pos should be 0-23
 
-void tblSet(u_char pLast) { //time for dinner!
-     gTblPos += 6;
-     if(gTblPos > 23)
-       gTblPos -= 24;
+void tblSet(u_char p_last) { //time for dinner!
+     g_tbl_pos += 6;
+     if(g_tbl_pos > 23)
+       g_tbl_pos -= 24;
 
-     if      (pLast == (u_char) (g4ROLL[gTblPos]) )
-       gTblAdd=1;
-     else if(pLast == (u_char) (g4ROLL[gTblPos])>>8)
-       gTblAdd=2;
-     else if(pLast == (u_char) (g4ROLL[gTblPos])>>16)
-       gTblAdd=3;
-     else if(pLast == (u_char) (g4ROLL[gTblPos])>>24)
-       gTblAdd=4;
+     if      (p_last == (u_char) (G4ROLL[g_tbl_pos]) )
+       g_tbl_add=3;
+     else if(p_last == (u_char) (G4ROLL[g_tbl_pos])>>8)
+       g_tbl_add=1;
+     else if(p_last == (u_char) (G4ROLL[g_tbl_pos])>>16)
+       g_tbl_add=2;
+     else if(p_last == (u_char) (G4ROLL[g_tbl_pos])>>24)
+       g_tbl_add=6;
 
-     gTblPos += gTblAdd;
-     if(gTblPos > 23)
-       gTblPos -= 24;
-     if(gTblPos > 23) {
+     g_tbl_pos += g_tbl_add;
+     if(g_tbl_pos > 23)
+       g_tbl_pos -= 24;
+     if(g_tbl_pos > 23) {
        printf("ERROR! table pos outside bounds!\nEXITING!\n");
-       return 0;
+       return;
      }//if
 
-     //printf("new gTblPos is %d - output was %c\n", gTblPos, pLast);
+     //printf("new g_tbl_pos is %d - output was %c\n", g_tbl_pos, p_last);
 }//tblSet
 
 
 int main() {
 
-  char * fileBuf;
-  u_char * outBuf;
-  int fileSz=0, outCnt=0, i=0;
-  u_char dtDec[4]={0,0,0,0};//form byte by byte
-  FILE *readFile,*outFile;
+  char * file_buf;
+  u_char * out_buf, lst_nuc=0, dt_dec;
+  long long file_sz=0, i=0, k=0, n_cnt=1, bs_offset=0;
+  FILE *read_file,*out_file;
 
 /*
   printf("using table\n");
   for(i=0;i<24;i++) {
-   printf("%d = %c%c%c%c\n", i, g4ROLL[i]>>24, g4ROLL[i]>>16, g4ROLL[i]>>8, g4ROLL[i]);
+   printf("%lld = %c%c%c%c\n", i, G4ROLL[i]>>24, G4ROLL[i]>>16, G4ROLL[i]>>8, G4ROLL[i]);
   }//for 24 (i)
 */
 
-  readFile = fopen("/tmp/test.dec", "r");
-  fseek(readFile, 0L, SEEK_END);
-  fileSz = ftell(readFile);
-  rewind(readFile);
+  read_file = fopen("/tmp/test.dec", "r");
+  fseek(read_file, 0L, SEEK_END);
+  file_sz = ftell(read_file);
+  rewind(read_file);
 
-  fileBuf = malloc(fileSz+1);
-  outBuf  = malloc(fileSz/4+4);//not always even but more is fine
-  memset(fileBuf, 0x0, fileSz+1);
-  memset(outBuf, 0x0, fileSz/4+4);
-  fread(fileBuf, fileSz, 1, readFile);
+  file_buf = malloc(file_sz+1);
+  out_buf  = malloc(file_sz/4+4);//not always even but more is fine
+  memset(file_buf, 0x0, file_sz+1);
+  memset(out_buf, 0x0, file_sz/4+4);
+  fread(file_buf, file_sz, 1, read_file);
 
-  //printf("%s\n---EOF---\n", fileBuf);
+  //printf("%s\n---EOF---\n", file_buf);
 
-  for(i=0; i<fileSz/4; i++) {
-     //printf("filePos 'real byte' is %d\n", i);
-     dtDec[0] = fileBuf[i*4];
-     dtDec[1] = fileBuf[i*4+1];
-     dtDec[2] = fileBuf[i*4+2];
-     dtDec[3] = fileBuf[i*4+3];
-     //printf("data read...%c%c%c%c\n", dtDec[0],dtDec[1],dtDec[2],dtDec[3] );
+  for(i=0; (i+(bs_offset/4))<(file_sz/4); i++) {
+     for(k=0; k<4; k++) {
 
-     //block 1 (high) (0xFF, 0x00, 0x00, 0x00)
-     if(dtDec[0] == (u_char)((g4ROLL[gTblPos])) )
-       outBuf[outCnt] += 192;
-     else if(dtDec[0] == (u_char)((g4ROLL[gTblPos])>>8) )
-       outBuf[outCnt] += 128;
-     else if(dtDec[0] == (u_char)((g4ROLL[gTblPos])>>16) )
-       outBuf[outCnt] += 64;
-     tblSet(dtDec[0]);
+          dt_dec = file_buf[i*4+k+bs_offset];
+          if(n_cnt >= HOMO_REPEAT) {
+            lst_nuc = dt_dec;
+            tblSet(lst_nuc);
+            bs_offset++;
+            n_cnt = 1;
+            k--;     //go back a chunk
+            continue;//skip the bs value
+          }
 
+          //printf("data read...%c\n", dt_dec);
+          if(lst_nuc == dt_dec)
+            n_cnt++;
+          else
+            n_cnt = 1;
+          lst_nuc = dt_dec;
 
-     //block 2 (high) (0x00, 0xFF, 0x00, 0x00)
-     if(dtDec[1] == (u_char)((g4ROLL[gTblPos])) )
-       outBuf[outCnt] += 48;
-     else if(dtDec[1] == (u_char)((g4ROLL[gTblPos])>>8) )
-       outBuf[outCnt] += 32;
-     else if(dtDec[1] == (u_char)((g4ROLL[gTblPos])>>16) )
-       outBuf[outCnt] += 16;
-     tblSet(dtDec[1]);
+          //           k == 0- 1- 2- 3
+          //decode ACGT to 00-00-00-00 byte, chunk by chunk
+          if(dt_dec == (u_char)((G4ROLL[g_tbl_pos])) )
+            out_buf[i] += (u_char)(3<<(2*(3-k)));//192,48,12,3
+          else if(dt_dec == (u_char)((G4ROLL[g_tbl_pos])>>8) )
+            out_buf[i] += (u_char)(2<<(2*(3-k)));//128,32,8,2
+          else if(dt_dec == (u_char)((G4ROLL[g_tbl_pos])>>16) )
+            out_buf[i] += (u_char)(1<<(2*(3-k)));//64,16,4,1
+          if(n_cnt < HOMO_REPEAT)
+            tblSet(lst_nuc);
+     }//k
 
-
-     //block 3 (low) (0x00, 0x00, 0xFF, 0x00)
-     if(dtDec[2] == (u_char)((g4ROLL[gTblPos])) )
-       outBuf[outCnt] += 12;
-     else if(dtDec[2] == (u_char)((g4ROLL[gTblPos])>>8) )
-       outBuf[outCnt] += 8;
-     else if(dtDec[2] == (u_char)((g4ROLL[gTblPos])>>16) )
-       outBuf[outCnt] += 4;
-     tblSet(dtDec[2]);
-
-
-     //block 4 (low) (0x00, 0x00, 0x00, 0xFF)
-     if(dtDec[3] == (u_char)((g4ROLL[gTblPos])) )
-       outBuf[outCnt] += 3;
-     else if(dtDec[3] == (u_char)((g4ROLL[gTblPos])>>8) )
-       outBuf[outCnt] += 2;
-     else if(dtDec[3] == (u_char)((g4ROLL[gTblPos])>>16) )
-       outBuf[outCnt] += 1;
-
-     //printf("last output formatted = %c =\n",outBuf[outCnt] );
-     /*
-      this should make data repeats longer for DNA structure.
-      if needed, you could shuffle the 'key' table for even
-      more repeat avoidance, while being able to encode/decode.
-
-      i looked at another 'proven' method and there are repeats
-      like this produces in the same encoded data.
-      shuffle is likely a waste
-
-      mathmatical probablity could produce repeats
-     */
-
-     outCnt++;
-     if(outCnt % 24) //4 * 6 = 24 bases
-       gTblPos += 7;
-     if(gTblPos>23)
-       gTblPos -= 24;
-
-     tblSet(dtDec[3]);
+     //same roll 'randomness' as encode
+     if(i % 24)
+       g_tbl_pos += 7;
+     if(g_tbl_pos>23)
+       g_tbl_pos -= 24;
 
   };//i, file
 
-  printf("outputting file to /tmp/output with size %d\n", fileSz/4);
-  outFile = fopen("/tmp/output", "wb");
-  fwrite(outBuf, 1, fileSz/4, outFile);
-
-  free(fileBuf);
-  fclose(readFile);
-  free(outBuf);
-  fclose(outFile);
+  printf("outputting file to /tmp/output with size %lld\n", file_sz/4);
+  printf("had %lld junk nucleotides\n", bs_offset);
+  out_file = fopen("/tmp/output", "wb");
+  fwrite(out_buf, 1, (file_sz/4) - (bs_offset/4), out_file);
+  //                               ^okay if this hits x.25 ?
+  free(file_buf);
+  fclose(read_file);
+  free(out_buf);
+  fclose(out_file);
   return 0;
 
 }//main
